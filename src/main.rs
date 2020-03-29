cafae91336f17d5779615ea2d0f21d42ce1f0ec2
Terry Chia <terrycwk1994@gmail.com>
2020-03-29 19:32:17 +0800

    Add basic command line argument parsing.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::{Arg, ArgGroup, App, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new(clap::crate_name!())
+        .author(clap::crate_authors!())
+        .version(clap::crate_version!())
+        .about(clap::crate_description!())
+        .subcommand(SubCommand::with_name("fetch")
+            .about("Download Frida artifacts.")
+            .arg(Arg::with_name("FRIDA-VERSION")
+                 .help("Download the specified Frida version.")
+                 .long("frida-version")
+                 .takes_value(true)))
+    .get_matches();
+
+    if let Some(version) = matches.value_of("FRIDA-VERSION") {
+        println!("--frida-version option not implemented yet.");
+    } else {
+        fetch_latest_release();
+    }
+}
+
+fn fetch_latest_release() {
+    ()
 }
